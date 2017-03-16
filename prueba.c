c02b878b8c369343288e144f727ad5fdb78647af
Óscar García <oscar.gpoblacion@uah.es>
2017-03-16 11:38:39 +0100

    Versión inicial de prueba

---
+++ prueba.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+    int i=4;
+    int j=23;
+
+    printf("Hola mundo\n");
+    printf("i^j = %d^%d = %d\n", i,j, pow(i,j));
+	return (0);
+}
